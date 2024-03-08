e76871e1d037262de8cff98df308f254ea37f553
amit <kumaramit28538@gmail.com>
2024-03-08 17:53:46 +0530

    Added conditinal statemt if and switch case

---
+++ Control Flow/conditionalStatement.js
@@ -0,0 +1,40 @@
+ // conditinal statemente
+
+// if
+//else
+//else if
+
+let a = 10;
+let b = 20;
+if (a > b) {
+  console.log("a is grator then b");
+} else if (a < b) {
+  console.log(" a is less than  b");
+} else {
+    console.log(first);
+}
+
+let time= 10;
+let greeting;
+
+if(time<10){
+    greeting="Good Morning"
+} else {
+     greeting="Good Evening"
+}
+console.log(greeting)
+
+// switch statement
+let text;
+let  x='0';
+switch(x) {
+  case 0:
+    text = "off";
+    break;
+  case 1:
+    text = "on";
+    break;
+  default:
+    text = "fuck off";
+}
+console.log(text)
